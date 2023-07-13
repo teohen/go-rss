4374e4cc1e3c595f0b05488dab3d8b9c9f1607f1
teteo <teodorohenriquecarvalho@gmail.com>
2023-07-12 21:01:24 -0300

    feat: adds the blog posts

--- feeds_db.go
+++ feeds_db.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
+	"sort"
+	"time"
 )
 
 type DBFeed struct {
@@ -10,12 +12,13 @@ type DBFeed struct {
 }
 
 type FeedsDBModel struct {
-	Id        string
-	CreatedAt string
-	UpdatedAt string
-	Name      string
-	URL       string
-	UserId    string
+	Id            string
+	CreatedAt     string
+	UpdatedAt     string
+	Name          string
+	URL           string
+	UserId        string
+	LastFetchedAt string
 }
 
 func (dbFeed *DBFeed) save(feed FeedsDBModel, dbOperator *DB) (FeedsDBModel, error) {
@@ -51,4 +54,52 @@ func (dbFeed *DBFeed) getAll(dbOperator *DB) ([]FeedsDBModel, error) {
 	}
 
 	return dbFeed.collection, nil
+}
+
+func (dbFeed *DBFeed) getNextFeedsToFetch(limit int, dbOperator *DB) ([]FeedsDBModel, error) {
+	feedsToFetch := []FeedsDBModel{}
+
+	allFeeds, err := dbFeed.getAll(dbOperator)
+
+	if err != nil {
+		fmt.Println(fmt.Printf("ERROR: %s", err))
+		return feedsToFetch, errors.New("error trying to get feeds to fetch")
+	}
+
+	sort.Slice(allFeeds, func(i, j int) bool {
+		if allFeeds[i].LastFetchedAt == "" {
+			return false
+		}
+		if allFeeds[j].LastFetchedAt == "" {
+			return true
+		}
+		return allFeeds[i].LastFetchedAt > allFeeds[j].LastFetchedAt
+	})
+
+	return allFeeds[:limit], nil
+}
+
+func (dbFeed *DBFeed) markFeedAsFetched(idFeed string, dbOperator *DB) (bool, error) {
+
+	if !dbOperator.isConnected() {
+		dbOperator.connect()
+	}
+
+	msg, err := dbOperator.execute(idFeed, "update feed")
+
+	if err != nil {
+		fmt.Println(fmt.Printf("ERROR: %s", err))
+		return false, errors.New("error trying to update feeds")
+	}
+
+	fmt.Println(fmt.Sprintf("SUCCESS: %s", msg))
+
+	for i, feed := range dbFeed.collection {
+		if feed.Id == idFeed {
+			dbFeed.collection[i].LastFetchedAt = time.Now().String()
+			return true, nil
+		}
+	}
+
+	return false, errors.New("feed not found on db")
 }
